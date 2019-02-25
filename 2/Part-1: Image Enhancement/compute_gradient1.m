bb6273223783b3265903ac9b9442cad905d1d2d2
Dragobloody <grama.dragos.ioan@gmail.com>
2019-02-25 21:09:31 +0100

    Update compute_gradient1.m

--- 2/Part-1: Image Enhancement/compute_gradient1.m
+++ 2/Part-1: Image Enhancement/compute_gradient1.m
@@ -1,6 +1,6 @@
 function [Gx, Gy, im_magnitude,im_direction] = compute_gradient(image)
   fprintf('Not implemented\n')
-  fprintf('BAG PUL IN FISIERUL ASTA!!!\n')
+  fprintf('BAG PULA IN FISIERUL ASTA!!!\n')
 
 end
 
