7360c5a3063104362435d4b87fa757f4f8b91f1d
Apostol Andrei <apo.andrei23@yahoo.com>
2019-02-26 22:45:29 +0100

    replaced createGabor with element-wise multiplication

--- 2/Part-1: Image Enhancement/gauss2D.m
+++ 2/Part-1: Image Enhancement/gauss2D.m
@@ -3,4 +3,7 @@ function G = gauss2D( sigma , kernel_size )
     x = kernel_size(1)
     y = kernel_size(2)
     G = gauss1D(sigma,x).'*gauss1D(sigma,y);
+    figure;
+    subplot(111);
+    imshow(G);
 end
