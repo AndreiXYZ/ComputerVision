d61be3024ea008ba1b5acc69f5ccba1a6cc348a6
TraianVidrascu <ntvidrascu@gmail.com>
2019-03-06 18:25:29 +0100

    update main

--- 3/main.m
+++ 3/main.m
@@ -1,12 +1,8 @@
 toy = imread('person_toy/00000001.jpg');
 pingpong = imread('pingpong/0000.jpeg');
-
-[H, c1] = harris_corner_detector(toy, 1e-6);
-[H, c2] = harris_corner_detector(pingpong, 1e-6);
-
-rot_toy = imread('00000001rot.jpg');
-[H, c1rot] = harris_corner_detector(rot_toy, 1e-6);
-%seems to be rotation invariant for 90 degrees
+treshold = 1e-6;
+[H, c1] = harris_corner_detector(toy, treshold);
+[H, c2] = harris_corner_detector(pingpong, treshold);
 
 %% Lucas-Kanade shpere
 image1 = imread('sphere1.ppm');
