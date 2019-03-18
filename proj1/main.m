bede0aabb899c7713ce658237d1847c1dc341500
Unknown <grama.dragos.ioan@gmail.com>
2019-03-18 13:10:27 +0100

    Main changes

--- proj1/main.m
+++ proj1/main.m
@@ -1,6 +1,10 @@
 %% Set random seed to make our experiments reproductible and load vlfeat library
 rng(42);
+% Linux path(Andrei and Stefan)
 run('./vlfeat-0.9.21/toolbox/vl_setup');
+
+% Dragos path(windows)
+run('C:/Program Files/MATLAB/R2018b/vlfeat-0.9.21-bin/toolbox/vl_setup')
 
 %% Remove unwanted classes from dataset and save result to disk. Only need to run once
 %remove_classes();
@@ -18,23 +22,25 @@ load('stl10_matlab/grayscale_sifts.mat');
 %% Now build the bag of visual words from the rest of the images. This will be used to train our svm
 
 load('stl10_matlab/centroids1000.mat');
-num_train_images = size(X_train, 1);
+load('stl10_matlab/train_curated.mat');
+num_train_imgs = size(X_train, 1);
 
 %Use the other half of  the images to build bovw
 size(train_features)
 size(num_train_imgs)
-for i=(num_train_images/2)+1:num_train_imgs
+imghists = zeros(num_train_imgs/2,length(centroids));
+for i=(num_train_imgs/2)+1:num_train_imgs
     img = X_train(i,:);
     img = unflatten_image(img);
     img = single(rgb2gray(im2double(img)));
     [f,d] = vl_sift(img);
     d = double(d');
-    indexes = knnsearch(train_features, d);
+    indexes = knnsearch(centroids, d);
     %Each row of indexes contains the index of the nearest
     %neighbor in train_features for the corresponding row in d.
     %Now encode each image as a bag of words.
-    idx = i-num_train_images/2;
-    imghists(idx,indexes) = 1;
+    idx = i-num_train_imgs/2;
+    imghists(idx,indexes) = imghists(idx,indexes) + 1;
 end
 
 size(imghists)
