9336c8df27be497399af8eb376393348943fe99a
Dragobloody <grama.dragos.ioan@gmail.com>
2019-04-03 15:26:15 +0200

    Finished

--- proj1/training_SVM.m
+++ proj1/training_SVM.m
@@ -1,10 +1,12 @@
 function models = training_SVM(imghists,labels)
     classes = [1,2,3,7,9];
     for i=1:5
-        binary_labels = double(labels == classes(i));  
+        % get binary labels
+        binary_labels = double(labels == classes(i)); 
+        % train SVM  with cross-validation
         aux = train(binary_labels, sparse(imghists) ,'-C -s 2');
         models(i) = train(binary_labels, sparse(imghists) , sprintf('-c %f -s 2', aux(1))); 
-        %models(i) = train(binary_labels, sparse(imghists), '-s 2');
+       
     end    
     
 end
