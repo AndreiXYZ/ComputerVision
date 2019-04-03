35940dd6585983d3b23e3eb20ae92f1e11d9327c
Dragobloody <grama.dragos.ioan@gmail.com>
2019-04-03 15:24:30 +0200

    Finished

--- proj1/classification.m
+++ proj1/classification.m
@@ -1,11 +1,10 @@
 function scores = classification(imghists, labels, models)
-    classes = [1,2,3,7,9];
-    ground_truth = [];
-    predicted = [];
-    accuracies = [];
+    classes = [1,2,3,7,9];  
     scores = [];
     for i=1:length(models)
+        % get binary labels
         binary_labels = double(labels == classes(i));
+        % calculate the confidence score of the classifier
         [~, ~,  score] = predict(binary_labels, sparse(imghists), models(i));
         if models(i).Label(1) == 0
             score = -score; 
